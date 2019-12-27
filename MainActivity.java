4f1c563d6897683457a98e5ca24d989832ff56ad
su1715 <58678617+su1715@users.noreply.github.com>
2019-12-27 16:45:37 +0900

    Issue

--- MainActivity.java
+++ MainActivity.java
@@ -1,8 +1,19 @@
 package com.example.test1;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.ContactsContract;
 import android.widget.TabHost;
 import android.os.Bundle;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,6 +27,20 @@ public class MainActivity extends AppCompatActivity {
         // 첫 번째 Tab. (탭 표시 텍스트:"TAB 1"), (페이지 뷰:"content1")
         TabHost.TabSpec ts1 = tabHost1.newTabSpec("Tab Spec 1") ;
         ts1.setContent(R.id.content1) ;
+
+
+
+        ArrayList<PhoneBook> phoneBooks=new ArrayList<>();
+        phoneBooks=getContacts(context);
+
+        // 리사이클러뷰에 LinearLayoutManager 객체 지정.
+        RecyclerView recyclerView = findViewById(R.id.recycler1) ;
+        recyclerView.setLayoutManager(new LinearLayoutManager(this)) ;
+
+        // 리사이클러뷰에 SimpleTextAdapter 객체 지정.
+        SimpleTextAdapter adapter = new SimpleTextAdapter(phoneBooks) ;
+        recyclerView.setAdapter(adapter) ;
+
         ts1.setIndicator("TAB 1") ;
         tabHost1.addTab(ts1)  ;
 
@@ -32,5 +57,64 @@ public class MainActivity extends AppCompatActivity {
         tabHost1.addTab(ts3) ;
 
 
+    }
+}
+class PhoneBook {
+    private String id;
+    private String name;
+    private String number;
+
+    public String getNumber() {
+        return number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public List<PhoneBook> getContacts(Context context) {
+        List<PhoneBook> datas = new ArrayList<>();
+        ContentResolver resolver = context.getContentResolver();
+        Uri phoneUri = ContactsContract.CommonDataKinds.Phone.CONTENT_URI;
+        String[] projection = {ContactsContract.CommonDataKinds.Phone.CONTACT_ID,
+                ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME,
+                ContactsContract.CommonDataKinds.Phone.NUMBER};
+
+        Cursor cursor = resolver.query(phoneUri, projection, null, null, null);
+        if (cursor != null) {
+            while (cursor.moveToNext()) {
+                int idIndex = cursor.getColumnIndex(projection[0]);
+                int nameIndex = cursor.getColumnIndex(projection[1]);
+                int numberIndex = cursor.getColumnIndex(projection[2]);
+                String id = cursor.getString(idIndex);
+                String name = cursor.getString(nameIndex);
+                String number = cursor.getString(numberIndex);
+
+                PhoneBook phoneBook = new PhoneBook();
+                phoneBook.setId(id);
+                phoneBook.setName(name);
+                phoneBook.setNumber(number);
+
+                datas.add(phoneBook);
+            }
+        }
+        cursor.close();
+        return datas;
     }
 }
